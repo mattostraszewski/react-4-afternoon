072cd9289b7c03a98e9fa8111c1a69424d5fb132
mattostraszewski <mrostraszewski@gmail.com>
2020-06-01 21:04:16 -0600

    halfway

--- src/components/ClassList/ClassList.js
+++ src/components/ClassList/ClassList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class ClassList extends Component {
   constructor() {
     super()
-    
+
+    this.state = {
+      students: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:3005/students?class=${this.prop.match.params.class}') //help meeeee!
+      .then(results => {
+        this.setState({
+          students: results.data
+        });
+      });
   }
 
   render() {
